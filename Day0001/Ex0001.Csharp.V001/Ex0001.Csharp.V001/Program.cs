9d7257bf5cd1d2d444da82143157812d341f22a5
unknown <mvarela@.contentproject.com>
2011-12-14 13:29:55 -0600

    * Deliberate Practice, Day 0001, C#, The 3n + 1 Problem

---
+++ Day0001/Ex0001.Csharp.V001/Ex0001.Csharp.V001/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex0001.Csharp.V001
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ConsoleKeyInfo keyInfo;
+			do
+			{
+				Console.WriteLine("Enter initial number:");
+				var initialNumberString = Console.ReadLine();
+				Console.WriteLine("Enter final number:");
+				var finalNumberString = Console.ReadLine();
+
+				int initialNumber, finalNumber;
+				var parsingSucceeded = int.TryParse(initialNumberString, out initialNumber);
+				parsingSucceeded &= int.TryParse(finalNumberString, out finalNumber);
+
+				if (!parsingSucceeded)
+				{
+					Console.WriteLine("Input is not valid, unable to parse one of the numbers, please make sure the numbers entered as valid integers.");
+					return;
+				}
+
+				if (initialNumber > finalNumber)
+				{
+					var auxiliaryNumber = finalNumber;
+					finalNumber = initialNumber;
+					initialNumber = auxiliaryNumber;
+				}
+
+				var maxCycleLenght = GetMaxCycleLenght(initialNumber, finalNumber);
+
+				Console.WriteLine(string.Format("Initial number: {0} | final number: {1} | max cycle lenght: {2}", initialNumber, finalNumber, maxCycleLenght));
+				Console.WriteLine("Press Escape to exit, any other key to continue.");
+				keyInfo = Console.ReadKey();
+
+			} while (keyInfo.Key != ConsoleKey.Escape);
+		}
+
+		static int GetMaxCycleLenght(int minTarget, int maxTarget)
+		{
+			var maxCycleLenght = 0;
+			for (int i = minTarget; i <= maxTarget; i++)
+			{
+				var currentCycleLenght = GetCycleLenght(i);
+				if (currentCycleLenght > maxCycleLenght)
+				{
+					maxCycleLenght = currentCycleLenght;
+				}
+			}
+			return maxCycleLenght;
+		}
+
+		static int GetCycleLenght(long target)
+		{
+			var originalTarget = target;
+			var cycleLenght = 0;
+			while (target != 1)
+			{
+				if (target % 2 == 0)
+				{
+					target /= 2;
+				}
+				else
+				{
+					target = (target * 3) + 1;
+				}
+				cycleLenght++;
+				if (target < 1)
+				{
+					throw new InvalidOperationException(string.Format("Target number {0} is invalid for this operation, the result of the operation was less than 1", originalTarget));
+				}
+			}
+			return ++cycleLenght;
+		}
+
+	}
+}
---
+++ Day0001/Ex0001.Csharp.V001/Ex0001.Csharp.V001/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ex0001.Csharp.V001")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ex0001.Csharp.V001")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("85182d56-676c-4269-a7b2-19aa70c428ef")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
